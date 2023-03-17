d5a822f83cb3389d6f09ac26bccb19f2bb61505b
tylerjusfly <tylerjusfly1@gmail.com>
2023-03-17 17:14:20 +0100

    fixed lorem ipsum on my landing page

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,27 +1,17 @@
 <template>
   <section class="text-white body-font">
-    <div
-      class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
-    >
-      <div
-        class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
-      >
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
           Alt School GitHub API <br class="hidden lg:inline-block" />
         </h1>
         <p class="mb-8 leading-relaxed">
-          Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-          plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-          mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon
-          try-hard chambray.
+          Hello, My name is Taiwo Momoh. I'm a software developer currently in the software engineering field. My tech stack are nodeJs
+          [NestJs, ExpressJs], reactJs , vueJs
         </p>
       </div>
       <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-        <img
-          class="object-cover object-center rounded"
-          alt="hero"
-          src="https://avatars.githubusercontent.com/u/53145644?v=4"
-        />
+        <img class="object-cover object-center rounded" alt="hero" src="https://avatars.githubusercontent.com/u/53145644?v=4" />
       </div>
     </div>
   </section>
